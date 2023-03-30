a63872b03e6b8b3e6313620cbcae5991db590b4d
MariaFariss <maria.fariss.etu@univ-lemans.fr>
2023-03-31 00:26:21 +0200

    respawnGhoul

--- Assets/Scripts/Explode.cs
+++ Assets/Scripts/Explode.cs
@@ -5,14 +5,27 @@ using UnityEngine;
 public class Explode : MonoBehaviour
 {
     public GameObject explosion;
+    public GameObject enemyToSpawn;
+    Vector3 killPos;
+    Quaternion killRot;
+    public float waitTime = 3.0f;
 
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.transform.tag == "ghoul")
         {
             Destroy(collision.transform.gameObject); // destroy the ghoul
+            killPos = collision.transform.position;
+            killRot = collision.transform.rotation;
+            StartCoroutine(SpawnEnemyAgain());
             Instantiate(explosion, collision.transform.position, collision.transform.rotation);
         }
+    }
+    IEnumerator SpawnEnemyAgain()
+    {
+        yield return new WaitForSeconds(waitTime);
+        Instantiate(enemyToSpawn, killPos, killRot);
+
     }
 }
 
